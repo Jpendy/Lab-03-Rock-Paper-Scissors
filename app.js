599bb749f7c6d8eaa036f6fa0df23eb7d00d0cb0
Jpendy <Jpendy256@gmail.com>
2020-04-01 16:37:31 -0700

    added losses and draws counter and reset button

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const playButton = document.getElementById('play-button');
 const computerThrowSpan = document.getElementById('computer-throw');
 const gamesPlayedSpan = document.getElementById('games-played');
 const gamesWonSpan = document.getElementById('games-won');
+const gamesLostSpan = document.getElementById('games-lost');
+const drawsSpan = document.getElementById('draws');
+const resetButton = document.getElementById('reset-button');
 
 randomThrow();
 let gamesPlayed = 0;
 let gamesWon = 0;
-computerThrowSpan.value = 0;
+let gamesLost = 0;
+let draws = 0;
+
 
 playButton.addEventListener('click', () => {
     const selectedRadioButton = document.querySelector('input[type=radio]:checked');
@@ -17,7 +22,7 @@ playButton.addEventListener('click', () => {
 
     let computerThrow = randomThrow();
 
-
+    computerThrowSpan.textContent = computerThrow;
     
     compareResults(userChoice, computerThrow);
 
@@ -25,9 +30,24 @@ playButton.addEventListener('click', () => {
         gamesWon++;
         gamesWonSpan.textContent = gamesWon;
     }
+    else if (compareResults(userChoice, computerThrow) === 'lose') {
+        gamesLost++;
+        gamesLostSpan.textContent = gamesLost;
+    }
+    else {
+        draws++;
+        drawsSpan.textContent = draws;
+    }
+
     gamesPlayed++;
 
     gamesPlayedSpan.textContent = gamesPlayed;
 
-})
+});
 
+resetButton.addEventListener('click', () => {
+    gamesPlayedSpan.textContent = '0';
+    gamesWonSpan.textContent = '0';
+    gamesLostSpan.textContent = '0';
+    drawsSpan.textContent = '0';
+});
