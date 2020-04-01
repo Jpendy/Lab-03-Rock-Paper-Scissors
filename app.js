0edb6020d71fe5284ef23ba4d00ec0aeb482a30e
Jpendy <Jpendy256@gmail.com>
2020-04-01 14:45:51 -0700

    initial setup complete

---
+++ app.js
@@ -0,0 +1,4 @@
+const playButton = document.getElementById('play-button');
+const computerThrowSpan = document.getElementById('computer-throw');
+const gamesPlayedSpan = document.getElementById('games-played');
+const gamesWonSpan = document.getElementById('games-won');
